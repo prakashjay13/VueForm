2d0ab564c0ec41aedcabe6bf158f0323478e9ecd
prakashjay13 <jayprakash.singh@neosoftmail.com>
2022-01-27 13:51:55 +0530

    VueForm

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+    <Registration/>
+  </div>
+</template>
+
+<script>
+import Registration from './components/Registration.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Registration
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
